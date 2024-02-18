c3a486cc50a0e01dcd665a85b98090dcf7619397
Lars Haavaldsen <lars.haavaldsen591@myci.csuci.edu>
2024-02-17 16:48:05 -0800

    Adding webhook container

---
+++ Webhook Container/README.md
@@ -0,0 +1,19 @@
+# About
+
+Container for adding/tracking challenges in our environment
+
+# Adding challenges -
+
+You can use this template for adding challenges -
+
+```
+    {
+      "name": "challenge_endpoint",
+      "points": 100,
+      "completed": false
+    }
+```
+
+The endpoint name is everything found after the /, i.e. http://container-ip/{NAME}
+
+# NOTE - do not push to repo with challenges set to true in db.json please:)
---
+++ Webhook Container/index.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+const PORT = 3000;
+
+function challengeComplete(path) {
+  path = path.slice(1); // remove '/'
+  // open our db.json and load
+  fs.readFile("db.json", "utf8", (err, data) => {
+    if (err) {
+      console.error("Error reading file:", err);
+      return;
+    }
+    // convert to json
+    var challengesData = JSON.parse(data);
+
+    var challenges = challengesData.challenges;
+
+    // get index of challenge from path
+    var challengeIndex = challenges.findIndex(
+      (challenge) => challenge.name === path
+    );
+    // update completion status
+    if (challengeIndex !== -1) {
+      challenges[challengeIndex].completed = true;
+    } else {
+      console.error("Challenge not found");
+      return;
+    }
+
+    // write updated db
+    fs.writeFile(
+      "db.json",
+      JSON.stringify(challengesData, null, 2),
+      "utf8",
+      (err) => {
+        if (err) {
+          console.error("Error writing to file:", err);
+          return;
+        }
+
+        console.log(path + " marked as completed, and data written to file.");
+      }
+    );
+  });
+}
+
+app.get("/*", (req, res) => {
+  res.status(200);
+  res.send();
+  challengeComplete(String(req.path));
+});
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running, http://localhost:" + PORT);
+  else console.log("Error occurred, server can't start", error);
+});
